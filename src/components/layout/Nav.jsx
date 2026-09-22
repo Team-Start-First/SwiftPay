762e74756dfbb64ade0ac70930d388e80b0bc826
Ene Alexander <ene875688@gmail.com>
2026-09-22 16:11:17 +0100

    updating cards page

--- src/components/layout/Nav.jsx
+++ src/components/layout/Nav.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import AOS from "aos";
 import "aos/dist/aos.css";
 import { motion, AnimatePresence } from "framer-motion";
 import { Link } from "react-router-dom";
